778e29e390dc8a92b4f69649b0b73a94abba1a7e
Jon Black <jon_black@mm.st>
2014-12-27 10:51:11 +0100

    Add lessons 1-4

---
+++ src/bin/lesson1.rs
@@ -0,0 +1,28 @@
+extern crate sdl2;
+
+use sdl2::video::{Window,WindowPos,OPENGL};
+use sdl2::timer::delay;
+use sdl2::pixels::Color;
+
+fn main() {
+    sdl2::init(sdl2::INIT_EVERYTHING);
+
+    let window = match Window::new("lesson 1", WindowPos::PosCentered,
+                                   WindowPos::PosCentered, 640, 480, OPENGL) {
+        Ok(w)  => w,
+        Err(e) => panic!("Failed to create window: {}", e.to_string()),
+    };
+
+
+    let mut screen = match window.get_surface() {
+        Ok(s)  => s,
+        Err(e) => panic!("Failed to get screen surface: {}", e.to_string()),
+    };
+
+    screen.fill_rect(None, Color::RGB(255, 255, 255)).unwrap();
+    window.update_surface();
+
+    delay(2000);
+
+    sdl2::quit();
+}
---
+++ src/bin/lesson2.rs
@@ -0,0 +1,33 @@
+extern crate sdl2;
+
+use sdl2::video::{Window,WindowPos,OPENGL};
+use sdl2::surface::Surface;
+use sdl2::timer::delay;
+
+fn main() {
+    sdl2::init(sdl2::INIT_EVERYTHING);
+
+    let window = match Window::new("lesson 2", WindowPos::PosCentered,
+                                   WindowPos::PosCentered, 640, 480, OPENGL) {
+        Ok(w)  => w,
+        Err(e) => panic!("Failed to create window: {}", e.to_string()),
+    };
+
+
+    let screen = match window.get_surface() {
+        Ok(s)  => s,
+        Err(e) => panic!("Failed to get screen surface: {}", e.to_string()),
+    };
+
+    let image = match Surface::from_bmp(&Path::new("res/hello_world.bmp")) {
+        Ok(i)  => i,
+        Err(e) => panic!("Failed to create image surface: {}", e.to_string()),
+    };
+
+    screen.blit(&image, None, None);
+    window.update_surface();
+
+    delay(2000);
+
+    sdl2::quit();
+}
